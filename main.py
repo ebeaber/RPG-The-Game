9b43c51657999065962557741525e67489951ee9
Fridgemate <jwajerngian@gmail.com>
2017-06-23 10:20:06 -0400

    Character creation logic still in progress. Add: choose_race func choose_class func Next: Ability scores dict and func

--- main.py
+++ main.py
@@ -1,27 +1,7 @@
-# imports
-import character
-
-# initialize game
-# TODO Add Load Player Logic after file handler fix
-
-print('{0:=^90}'.format(' The RPG '))  # Game Title line in text mode
-print('{0:<90}'.format('Lets begin your adventure!'))  # Crappy welcome message
-
-x = character.get_player_class()  # create a variable for the subclasses of the Player object
-player1 = character.Player.factory(x)  # Call the Player Object Factory and create a player
-class_info = character.Player.info_factory(x)  # Fetch the information on the chosen character class for the player.
+ = character.Player.info_factory(x)  # Fetch the information on the chosen character class for the player.
 
 # Customize stats logic here
 # TODO Create the logic to have the player customize their stats and overwrite player key values
 
 # TODO Discuss the logical flow of character creation:
 """
-#1: 
-Should we first gather the stat numbers and then build the players Character class object?
-
-"""
-
-print(player1.__dict__)  # for testing purposes only
-
-# Save the data!
-# FIXME fix the file handler to correctly save the player data after creation
