6da851166a287a5bc2d0563ef72d24ae8bcbad2e
Fridgemate <jwajerngian@gmail.com>
2017-06-24 13:57:47 -0400

    Enemy Generator in progress.
    Working Dictionary comprehension to generate a random size enemy, generate stats for that enemy, add the size modifiers and return a enemy object that can be used by the combat module.

--- game_files/enemy_generator.py
+++ game_files/enemy_generator.py
@@ -22,29 +22,21 @@ size_modifiers['large'] = {'strength': 3, 'agility': -2, 'intelligence': -2,
                            'wisdom': -2, 'constitution': 3}
 size_modifiers['medium'] = {'strength': 1, 'agility': 1, 'intelligence': 1,
                            'wisdom': 1, 'constitution': 1}
-size_modifiers['large'] = {'strength': -2, 'agility': 3, 'intelligence': 2,
+size_modifiers['small'] = {'strength': -2, 'agility': 3, 'intelligence': 2,
                            'wisdom': 2, 'constitution': -1}
 size_modifiers['boss'] = {'strength': 5, 'agility': 5, 'intelligence': 5,
                            'wisdom': 5, 'constitution': 5}
 
 
+
+d = {k: v for k, v in enemies.items() if v == 'small'}
+
+print(d)
 # TODO FINISH THE RANDOM ENEMY LOGIC
 def random_enemy():
-    odds = random.randint(1, 100)
-    b = list(set(enemies.values()))
-    if odds >= 99:
-        return b[3]
+    odds = .99
+    ran = random.random()
+    if ran > odds:
+        enemy_list
     else:
-        return b[random.randint(0, 2)]
-
-
-count = 10000
-boss_hits = 0
-while count > 0:
-    a = random_enemy()
-    if a == "boss":
-        boss_hits += 1
-        print(a)
-    count -= 1
-
-print(boss_hits)
+        pass
