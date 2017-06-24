66207ff32eb8c9e40dff64493b4c45610e049400
Fridgemate <jwajerngian@gmail.com>
2017-06-24 16:52:12 -0400

    Enemy Generator in progress.
    Working Dictionary comprehension to generate a random size enemy, generate stats for that enemy, add the size modifiers and return a enemy object that can be used by the combat module.

--- game_files/enemy_generator.py
+++ game_files/enemy_generator.py
@@ -18,25 +18,40 @@ enemies = {'Spider': 'small', 'Imp': 'small', 'Wolf': 'medium', 'Goblin': 'mediu
 
 # Stat modifiers for enemy size
 size_modifiers = dict()
-size_modifiers['large'] = {'strength': 3, 'agility': -2, 'intelligence': -2,
-                           'wisdom': -2, 'constitution': 3}
-size_modifiers['medium'] = {'strength': 1, 'agility': 1, 'intelligence': 1,
-                           'wisdom': 1, 'constitution': 1}
-size_modifiers['small'] = {'strength': -2, 'agility': 3, 'intelligence': 2,
-                           'wisdom': 2, 'constitution': -1}
-size_modifiers['boss'] = {'strength': 5, 'agility': 5, 'intelligence': 5,
-                           'wisdom': 5, 'constitution': 5}
+size_modifiers['large'] = {'Strength': 3, 'Agility': -2, 'Intelligence': -2,
+                           'Wisdom': -2, 'Constitution': 3}
+size_modifiers['medium'] = {'Strength': 1, 'Agility': 1, 'Intelligence': 1,
+                            'Wisdom': 1, 'Constitution': 1}
+size_modifiers['small'] = {'Strength': -2, 'Agility': 3, 'Intelligence': 2,
+                           'Wisdom': 2, 'Constitution': -1}
+size_modifiers['boss'] = {'Strength': 5, 'Agility': 5, 'Intelligence': 5,
+                          'Wisdom': 5, 'Constitution': 5}
 
 
+def random_enemy_stats():
+    scores = sorted(dice.enemy_stat_roll())
+    stat_names = ['Strength', 'Agility', 'Intelligence', 'Wisdom', 'Constitution']
+    enemy_stats = dict()
+    for j in range(5):
+        enemy_modifier = size_modifiers[stat_names[j]]
+        thisscore = -99
+        while thisscore not in scores:
+            thisscore = scores[-1]
+        enemy_stats[stat_names[j]] = thisscore + enemy_modifier  # assign mods to score
+        score_index = scores.index(thisscore)
+        del scores[score_index]
+    return enemy_stats
 
-d = {k: v for k, v in enemies.items() if v == 'small'}
+stat_names = ['Strength', 'Agility', 'Intelligence', 'Wisdom', 'Constitution']
+print(size_modifiers['small'])
+print([size_modifiers['small'][stat_names[0]]])
 
-print(d)
-# TODO FINISH THE RANDOM ENEMY LOGIC
-def random_enemy():
-    odds = .99
-    ran = random.random()
-    if ran > odds:
-        enemy_list
+
+def ran_enemy_size():
+    x = (sorted(list(set(enemies.values()))))  # get all enemy sizes
+    boss_odds = .99
+    if random > boss_odds:
+        return x[0]
     else:
-        pass
+        return x[random.randint[1,3]]
+
