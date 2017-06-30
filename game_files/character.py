c589d4310f679ff67e8560d6d108151cee7d883a
Fridgemate <jwajerngian@gmail.com>
2017-06-30 08:09:42 -0400

    File Handler progress:
    Navigate to correct directory... done
    Verify directory exists... done
    Convert player object to JSON... done
    Write to file... done
    Read from file... done
    Create new object from data... in progress...

--- game_files/character.py
+++ game_files/character.py
@@ -186,7 +186,6 @@ def player_energy(agility, level):
 # build the player dictionary before passing to Object
 def build_player_object():
     # Gather's all the data to build a new player object.
-    print('{0:=^90}'.format(' RPG The Game '))
     p = dict()
     p['level'] = 1  # New Characters start at level 1
     p['experience'] = 0  # Dream on greenhorn.  0 XP to start
