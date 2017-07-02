e8902e1fc4c60a36854369376379851d4885f221
Joseph Jernigan <jwajernigan@gmail.com>
2017-07-02 18:25:38 -0400

    Updated __init__.py updated .gitignore

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-# Recreate main file after character logic repaired
-print('Create a game')
+import pygame
+import sys
+
+# init gui
+pygame.init()
+
+size = width, height = 800, 600
+speed = [2, 2]
+black = 255, 255, 255
+
+screen = pygame.display.set_mode(size)
+
+ball = pygame.image.load_basic('./assets/intro_ball.bmp')
+ballrect = ball.get_rect()
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+
+    screen.fill(black)
+    screen.blit(ball, ballrect)
