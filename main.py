faa2f63d666f11c229ec590b86085fb5ed2bb54f
Joseph Jernigan <jwajernigan@gmail.com>
2017-07-02 23:00:58 -0400

    New Branch for Main Alpha

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ print('\n{0:=^120}\n'.format(' RPG The Game '))
 # run the file handler script
 player = filehandler.game_load()
 
-print(player.__dict__)
+# for testing purposes, remove this line when done.
+print(player.__dict__)  # for testing purposes
